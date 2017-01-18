df3ddf0bf5148bb32548cb1053ce5f07de4006c8
Pietrangelo <StoneAngelTechnologiesLLC@gmail.com>
2017-01-17 21:41:39 -0700

    Add files via upload
    Application:  User enters average run-time(per mile). 
                         User enters there most recent running distance.
                         User enters the length of time taken to run their running distance.
                         Application processes user information.
                         Application returns(displays) distance of entered run in 'miles', 'meters' and 'feet'.
                         Application displays time difference per mile of the entered run to their average run.

---
+++ RunTracking/RunTracking/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunTracking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double runTimeGoal = 0;
+            double runTimeDistance = 0;
+            double runTime = 0;
+
+            runTimeGoal = GetDouble("Enter your average run time goal: ");
+            runTimeDistance = GetDouble("Enter your run time distance in miles: ");
+            runTime = GetDouble("Enter your run time: ");
+            Console.WriteLine();
+            Console.WriteLine("You ran {0} miles, {2} meters, or {1} feet", runTimeDistance,
+                                Utility.MileToFeet(runTimeDistance), Utility.MeterToMile(runTimeDistance));
+            Console.WriteLine("The goal differance per mile was: {0}",
+                Utility.GoalDifferance(runTimeGoal, runTimeDistance, runTime)); 
+            Console.ReadKey();
+        }
+
+        static double GetDouble(string question)
+        {
+            string input;
+            double number;
+            
+            Console.WriteLine(question + ": ");
+            input = Console.ReadLine();
+            // testing to see if input string can be converted to numbers
+            if (!double.TryParse(input, out number))
+            {
+
+                Console.WriteLine("invalad Entery, Number is zero");
+            }
+            return number;
+        }
+    }
+}
---
+++ RunTracking/RunTracking/Utility.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunTracking
+{
+    class Utility
+    {
+        const double feetInMile = 5280;
+        const double meterInMile = 1609.34;
+       
+        public static double MileToFeet(double mile)
+        {
+            return mile * feetInMile; 
+        }
+
+        public static double MeterToMile(double mile)
+        {
+            return (mile * meterInMile);
+        }
+
+        public static string GoalDifferance(double goal, double distance, double time)
+        {
+            double temp = 0;
+            int minute = 0;
+            int seconds = 0;
+            double differance;
+            string Answer;
+
+            temp = time / distance;
+            differance = temp - goal;
+            minute = Math.Abs((int)(differance));
+            differance -= (int)differance;
+            seconds = Math.Abs((int)(differance * 60));
+
+            if ((temp - goal) >= 0)
+
+                Answer = String.Format("+{0} minutes and {1} seconds", minute, seconds);
+            
+            else
+            
+                Answer = String.Format("-{0} minutes and {1} seconds", minute, seconds);
+            
+            return Answer;
+
+            
+        }
+    }
+}
